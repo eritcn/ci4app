b581176d0657844d9b631144bfa03a755c49c603
eritcn <erit.github@gmail.com>
2025-06-07 15:27:54 +0800

    committed

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -39,43 +39,66 @@
                             <div class="col-lg">
                                 <div class="p-5">
                                     <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                        <h1 class="h4 text-gray-900 mb-4"><?=lang('Auth.loginTitle')?></h1>
                                     </div>
-                                    <form class="user">
+
+                                    <?= view('Myth\Auth\Views\_message_block') ?>
+
+                                    <form action= "<?= route_to('login') ?>" method="post" class="user">
+                                      <?= csrf_field() ?>
+                                    
+                                    <?php if ($config->validFields === ['email']): ?>
                                         <div class="form-group">
-                                            <input type="email" class="form-control form-control-user"
-                                                id="exampleInputEmail" aria-describedby="emailHelp"
-                                                placeholder="Enter Email Address...">
+                                            <input type="email" class="form-control form-control-user <?php if (session('errors.login')) : ?>is-invalid<?php endif ?>"
+                                                name="login" 
+                                                placeholder="<?=lang('Auth.email')?>">
+                                            <div class="invalid-feedback">
+								              <?= session('errors.login') ?>
+							                </div>
                                         </div>
+                                    <?php else: ?>
                                         <div class="form-group">
-                                            <input type="password" class="form-control form-control-user"
-                                                id="exampleInputPassword" placeholder="Password">
+                                            <input type="text" class="form-control form-control-user <?php if (session('errors.login')) : ?>is-invalid<?php endif ?>"
+                                                name="login" placeholder="<?=lang('Auth.emailOrUsername')?>">
+                                                <div class="invalid-feedback">
+                                                    <?= session('errors.login') ?>
+                                                </div>
                                         </div>
+                                    <?php endif; ?>
+
+                                            <div class="form-group">
+                                            <input type="password" class="form-control form-control-user <?php if (session('errors.password')) : ?>is-invalid<?php endif ?>"
+                                                name="password" placeholder="<?=lang('Auth.password')?>">
+                                        </div>
+
+                                    <?php if ($config->allowRemembering): ?>
                                         <div class="form-group">
                                             <div class="custom-control custom-checkbox small">
-                                                <input type="checkbox" class="custom-control-input" id="customCheck">
-                                                <label class="custom-control-label" for="customCheck">Remember
-                                                    Me</label>
+                                                <input type="checkbox" class="custom-control-input " name="remembering" <?php if (old('remember')) : ?> checked <?php endif ?>> 
+                                                <label class="custom-control-label" for="customCheck">Remember Me</label>
                                             </div>
                                         </div>
-                                        <a href="index.html" class="btn btn-primary btn-user btn-block">
-                                            Login
-                                        </a>
-                                        <!-- <hr> -->
-                                        <!-- <a href="index.html" class="btn btn-google btn-user btn-block">
-                                            <i class="fab fa-google fa-fw"></i> Login with Google
-                                        </a> -->
-                                        <!-- <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                                            <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                                        </a> -->
+                                     <?php endif; ?>   
+
+                                        <button type="submit" class="btn btn-primary btn-user btn-block">
+                                            <?=lang('Auth.loginAction')?>
+                                        </button>
+                                
                                     </form>
                                     <hr>
+
+                                    <?php if ($config->activeResetter): ?>
                                     <div class="text-center">
-                                        <a class="small" href="forgot-password.html">Forgot Password?</a>
+                                        <a class="small" href="<?= route_to('forgot') ?>"><?=lang('Auth.forgotYourPassword')?></a>
                                     </div>
+                                    <?php endif; ?>
+
+
+                                    <?php if ($config->allowRegistration) : ?>
                                     <div class="text-center">
-                                        <a class="small" href="register.html">Create an Account!</a>
+                                        <a class="small" href="<?= route_to('register') ?>"><?=lang('Auth.needAnAccount')?></a>
                                     </div>
+                                    <?php endif; ?>
                                 </div>
                             </div>
                         </div>
