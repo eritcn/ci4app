8b52a4a51e0953f8311afd44382f0d7d71b390a9
eritcn <erit.github@gmail.com>
2025-09-01 07:46:43 +0800

    commit

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -9,7 +9,9 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Inst Login</title>
+    <title>Form Login</title>
+    
+    <link rel="icon" href="<?= base_url('favicon-new.ico') ?>" type="image/x-icon">
 
     <!-- Custom fonts for this template-->
     <link href="<?= base_url('vendor/fontawesome-free/css/all.min.css');?> "rel="stylesheet" type="text/css">
@@ -19,33 +21,49 @@
 
     <!-- Custom styles for this template-->
     <link href="<?= base_url('css/sb-admin-2.min.css');?> "rel="stylesheet">
+    
+     <style>
+      
+       .field-icon {
+    position: absolute;
+    top: 50%;
+    right: 15px;  /* jarak dari kanan */
+    transform: translateY(-50%);
+    cursor: pointer;
+    color: #6c757d; /* abu-abu biar soft */
+    font-size: 1.1rem;
+    }
+
+    </style>
 
 </head>
 
-<body class="bg-gradient-primary">
+<body class="bg-primary">
 
     <div class="container">
 
         <!-- Outer Row -->
         <div class="row justify-content-center">
 
-            <div class="col-md-6">
+            
 
                 <div class="card o-hidden border-0 shadow-lg my-5">
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
-                            <!-- <div class="col-lg-6 d-none d-lg-block bg-login-image"></div> -->
+                             <!--<div class="col-lg-6 d-none d-lg-block bg-login-image"></div> -->
                             <div class="col-lg">
                                 <div class="p-5">
                                     <div class="text-center">
-                                        <h3 class="h3 text-dark mb-2">Met dateng gaess...yuk kita</h3>
-                                        <h1 class="h4 text-gray-900 mb-4"> <?=lang('Auth.loginTitle')?></h1>
+                                         <img class="col-md-4"
+                                    src="<?= base_url('img/visitor.png'); ?>" >
+                                        <h5 class="h4 text-gray-700"> <b>Selamat Datang, Yuk </h5>
+                                        <h1 class="h4 text-gray-700 mb-4"> <?=lang('Auth.loginTitle')?></h1>
                                     </div>
 
                                     <?= view('Myth\Auth\Views\_message_block') ?>
 
-                                    <form action= "<?= url_to('login') ?>" method="post" class="user">
+                                    <form action= "<?= route_to('login') ?>" method="post" class="user">
                                       <?= csrf_field() ?>
                                     
                                     <?php if ($config->validFields === ['email']): ?>
@@ -53,24 +71,18 @@
                                             <input type="email" class="form-control form-control-user <?php if (session('errors.login')) : ?>is-invalid<?php endif ?>"
                                                 name="login" 
                                                 placeholder="<?=lang('Auth.email')?>">
-                                            <div class="invalid-feedback">
-								              <?= session('errors.login') ?>
-							                </div>
                                         </div>
                                     <?php else: ?>
                                         <div class="form-group">
                                             <input type="text" class="form-control form-control-user <?php if (session('errors.login')) : ?>is-invalid<?php endif ?>"
                                                 name="login" placeholder="<?=lang('Auth.emailOrUsername')?>">
-                                                <div class="invalid-feedback">
-                                                    <?= session('errors.login') ?>
-                                                </div>
                                         </div>
                                     <?php endif; ?>
 
-                                            <div class="form-group">
-                                            <input type="password" class="form-control form-control-user <?php if (session('errors.password')) : ?>is-invalid<?php endif ?>"
-                                                name="password" placeholder="<?=lang('Auth.password')?>">
-                                        </div>
+                                           <div class="form-group position-relative">
+                                              <input type="password" class="form-control form-control-user <?php if (session('errors.password')) : ?>is-invalid<?php endif ?>" name="password" id="password" 
+                                              placeholder="<?=lang('Auth.password')?>"><span toggle="#password" class="fa fa-fw fa-eye field-icon toggle-password"></span>
+                                           </div>
 
                                     <?php if ($config->allowRemembering): ?>
                                         <div class="form-group">
@@ -84,33 +96,35 @@
                                         <button type="submit" class="btn btn-primary btn-user btn-block">
                                             <?=lang('Auth.loginAction')?>
                                         </button>
-                                
                                     </form>
+                                    
                                     <hr>
 
-                                    <?php if ($config->activeResetter): ?>
-                                    <div class="text-center">
-                                        <a class="small" href="<?= url_to('forgot') ?>"><?=lang('Auth.forgotYourPassword')?></a>
-                                    </div>
-                                    <?php endif; ?>
+                                      <div class="d-flex justify-content-between mt-3">
+                                       <a class="small ml-3" href="<?= base_url('forgot') ?>">Lupa password?</a>
+                                       <span></span>
+                                       <a class="small mr-3" href="<?= url_to('register') ?>">Mau buat akun?</a>
+                                      </div>
 
-
-                                    <?php if ($config->allowRegistration) : ?>
-                                    <div class="text-center">
-                                        <a class="small" href="<?= url_to('register') ?>"><?=lang('Auth.needAnAccount')?></a>
-                                    </div>
-                                    <?php endif; ?>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-
-            </div>
-
-        </div>
-
-    </div>
+           </div>
+      </div>
+    
+       <script>
+        document.querySelectorAll(".toggle-password").forEach(function(toggle) {
+            toggle.addEventListener("click", function() {
+                const input = document.querySelector(this.getAttribute("toggle"));
+                const type = input.getAttribute("type") === "password" ? "text" : "password";
+                input.setAttribute("type", type);
+                this.classList.toggle("fa-eye-slash");
+                this.classList.toggle("fa-eye");
+            });
+        });
+    </script>
 
     <!-- Bootstrap core JavaScript-->
     <script src="<?= base_url('vendor/bootstrap/js/bootstrap.bundle.min.js');?>"></script>
